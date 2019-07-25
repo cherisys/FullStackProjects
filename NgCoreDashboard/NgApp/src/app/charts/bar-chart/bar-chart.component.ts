b62e5d676bd4675856e78958affce8fb9630d6ad
cherisys <arshad@cherisys.com>
2019-07-25 06:18:20 +0530

    Created orders table

--- NgCoreDashboard/NgApp/src/app/charts/bar-chart/bar-chart.component.ts
+++ NgCoreDashboard/NgApp/src/app/charts/bar-chart/bar-chart.component.ts
@@ -17,28 +17,11 @@ export class BarChartComponent implements OnInit {
     this.BarChart = new Chart('barChart', {
       type: 'bar',
     data: {
-     labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-     datasets: [{
-         label: '# of Votes',
-         data: [9,7 , 3, 5, 2, 10],
-         backgroundColor: [
-             'rgba(255, 99, 132, 0.2)',
-             'rgba(54, 162, 235, 0.2)',
-             'rgba(255, 206, 86, 0.2)',
-             'rgba(75, 192, 192, 0.2)',
-             'rgba(153, 102, 255, 0.2)',
-             'rgba(255, 159, 64, 0.2)'
-         ],
-         borderColor: [
-             'rgba(255,99,132,1)',
-             'rgba(54, 162, 235, 1)',
-             'rgba(255, 206, 86, 1)',
-             'rgba(75, 192, 192, 1)',
-             'rgba(153, 102, 255, 1)',
-             'rgba(255, 159, 64, 1)'
-         ],
-         borderWidth: 1
-     }]
+     labels: ["W1", "W2", "W3", "W4", "W5", "W6"],
+     datasets: [
+      {data: [9,7 , 3, 5, 2, 10], label: "Q3 Sales", backgroundColor: 'white', borderColor: 'silver'},
+      {data: [9,7 , 3, 5, 2, 10], label: "Q4 Sales", backgroundColor: 'yellow', borderColor: 'green'}
+     ]
     }, 
     options: {
      scales: {
--- NgCoreDashboard/NgApp/src/app/charts/pie-chart/pie-chart.component.ts
+++ NgCoreDashboard/NgApp/src/app/charts/pie-chart/pie-chart.component.ts
@@ -16,25 +16,18 @@ export class PieChartComponent implements OnInit {
     this.PieChart = new Chart('pieChart', {
       type: 'pie',
     data: {
-     labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+     labels: ["XYZ Logistics", "Main St. Bakery", "Acme Hosting"],
      datasets: [{
-         label: '# of Votes',
-         data: [9,7 , 3, 5, 2, 10],
+         data: [350,450,120],
          backgroundColor: [
-             'rgba(255, 99, 132, 0.2)',
-             'rgba(54, 162, 235, 0.2)',
-             'rgba(255, 206, 86, 0.2)',
-             'rgba(75, 192, 192, 0.2)',
-             'rgba(153, 102, 255, 0.2)',
-             'rgba(255, 159, 64, 0.2)'
+             'yellow',
+             'white',
+             'skyblue'
          ],
          borderColor: [
-             'rgba(255,99,132,1)',
-             'rgba(54, 162, 235, 1)',
-             'rgba(255, 206, 86, 1)',
-             'rgba(75, 192, 192, 1)',
-             'rgba(153, 102, 255, 1)',
-             'rgba(255, 159, 64, 1)'
+             'green',
+             'silver',
+             'purple'
          ],
          borderWidth: 1
      }]
